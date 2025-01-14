52785c79416e232932eaf4e89bb11b81de0545a6
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-01-14 08:38:48 +0530

    Added the load balancer for feign client

--- address-service/src/main/java/com/epam/address_service/controller/AddressController.java
+++ address-service/src/main/java/com/epam/address_service/controller/AddressController.java
@@ -2,6 +2,10 @@ package com.epam.address_service.controller;
 
 import com.epam.address_service.dto.AddressDTO;
 import com.epam.address_service.service.AddressService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "address")
 public class AddressController {
+
+    private static final Logger LOGGER = LogManager.getLogger(AddressController.class);
+
+    @Autowired
+    private Environment environment;
 
     private final AddressService addressService;
 
@@ -18,11 +27,13 @@ public class AddressController {
 
     @GetMapping(value = "{id}")
     public ResponseEntity<AddressDTO> getAddress(@PathVariable String id) {
+        LOGGER.info("Current port : {}", environment.getProperty("server.port"));
         return ResponseEntity.status(HttpStatus.OK).body(addressService.getAddressById(id));
     }
 
     @PostMapping
     public ResponseEntity<AddressDTO> saveAddress(@RequestBody AddressDTO addressDTO) {
+        LOGGER.info("Current port : {}", environment.getProperty("server.port"));
         return ResponseEntity.status(HttpStatus.CREATED).body(addressService.saveAddress(addressDTO));
     }
 }
--- employee-service/src/main/java/com/epam/employee_service/rest_client/AddressClientService.java
+++ employee-service/src/main/java/com/epam/employee_service/rest_client/AddressClientService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "address-service-client", url = "http://localhost:8082", path = "address-app/api/address")
+// replace the service name it will automatically balance the load
+@FeignClient(value = "ADDRESS-SERVICE", path = "address-app/api/address")
 public interface AddressClientService {
 
     @GetMapping(value = "/{id}")
